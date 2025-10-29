5874023bda9cd6933ff6badc3ce8e91a6ae4368c
Raihan Abadi <raihan.abadi@cakrawala.ac.id>
2025-10-29 11:08:15 +0700

    Solving Challange 2

--- python/challenges/2_condition_age.py
+++ python/challenges/2_condition_age.py
@@ -1,5 +1,17 @@
 # Program Klasifikasi Usia
 
 # 1. TODO: Baca input dari pengguna
+umur = int(input("Masukan Umur Anda : "))
+
+#Validasi umur
+if umur < 12:
+    kategori = "Anak Anak"
+elif umur < 17:
+    kategori = "Remaja"
+elif umur < 59:
+    kategori = "Dewasa"
+else:
+    kategori = "Lansia"
 
 # 2. TODO Tentukan kategori berdasarkan usia
+print("Katergori : ", kategori)
