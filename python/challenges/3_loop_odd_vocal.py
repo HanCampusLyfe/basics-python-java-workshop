14dab8e2c1188b5d3f16974dd5b3b2a7580ac260
Raihan Abadi <raihan.abadi@cakrawala.ac.id>
2025-10-29 11:17:35 +0700

    Solving Challange 3

--- python/challenges/3_loop_odd_vocal.py
+++ python/challenges/3_loop_odd_vocal.py
@@ -1,11 +1,19 @@
 # TODO: Program untuk mencetak bilangan ganjil dari 1 hingga 15
 odd_numbers = []
-print(odd_numbers)
+for i in range(1, 16):       # dari 1 sampai 15
+    if i % 2 != 0:           # jika sisa bagi 2 bukan 0 → ganjil
+        odd_numbers.append(i)
+
+print("Bilangan ganjil dari 1 sampai 15:", odd_numbers)
 
 # TODO: Program menghitung jumlah huruf vokal
 word = input("Masukkan kata: ").lower()
 
 vowels = "aiueo"
 count = 0
+
+for kata in word:
+    if kata in vowels:
+        count += 1
 
 print("Jumlah huruf vokal:", count)
